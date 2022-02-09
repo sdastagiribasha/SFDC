aae04707fdb1ab2ce4a4fe4b5f2b21aa5ecd620e
sdastagiribasha <80464654+sdastagiribasha@users.noreply.github.com>
2022-02-09 17:56:03 +0530

    Create TypesofarrayDeclaration.js

---
+++ Javascript/ArrayDeclaration/TypesofarrayDeclaration.js
@@ -0,0 +1,9 @@
+var arrayone=[1,2,3,4,5];
+console.log(arrayone);//-->1st type
+
+for(i=1;i<=10;i++)
+{
+	console.log(i);//-->2nd type
+}
+
+arrayone.forEach(arrele => console.log(arrele)); //-->3rdtype
